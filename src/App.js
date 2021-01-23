f9e63a7781ceee17c3870c668e81d5d84a315e77
Feisal Fahmi <feisalfahmi@gmail.com>
2021-01-23 18:00:57 +0700

    Upload Files

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic"></ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+});
+
+export default App;
